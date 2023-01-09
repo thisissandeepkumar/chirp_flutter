bacc19edea8558b34b551716d3ddd39c5fe363d1
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-01-09 12:42:44 +0530

    Added Bottom Navigation Bar

--- lib/pages/home/index.dart
+++ lib/pages/home/index.dart
@@ -1,3 +1,4 @@
+import 'package:comms_flutter/pages/profile/index.dart';
 import 'package:comms_flutter/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,6 +18,8 @@ class _HomePageState extends State<HomePage> {
 
   late TextEditingController searchController;
 
+  int _selectedPage = 0;
+
   @override
   void initState() {
     super.initState();
@@ -28,35 +31,41 @@ class _HomePageState extends State<HomePage> {
     width = MediaQuery.of(context).size.width;
     height = MediaQuery.of(context).size.height;
     return Scaffold(
+        bottomNavigationBar: homeNavigationBar(),
         body: MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(
-          value: AuthProvider.instance,
-        ),
-      ],
-      child: _mainUI(),
-    ));
+          providers: [
+            ChangeNotifierProvider.value(
+              value: AuthProvider.instance,
+            ),
+          ],
+          child: _mainUI(),
+        ));
   }
 
   Widget _mainUI() {
     return Builder(builder: (BuildContext innerContext) {
       authProvider = Provider.of<AuthProvider>(innerContext);
-      return Scaffold(
-        appBar: homeAppBar(),
-        body: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 10,
-              ),
-              child: searchBar(
-                searchController,
-              ),
+      switch (_selectedPage) {
+        case 0:
+          return Scaffold(
+            appBar: homeAppBar(),
+            body: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
+                  ),
+                  child: searchBar(
+                    searchController,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      );
+          );
+        default:
+          return const ProfilePage();
+      }
     });
   }
 
@@ -96,6 +105,33 @@ class _HomePageState extends State<HomePage> {
           borderRadius: BorderRadius.circular(20),
         ),
       ),
+    );
+  }
+
+  Widget homeNavigationBar() {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          label: "Messages",
+          icon: Icon(
+            Icons.message,
+          ),
+        ),
+        BottomNavigationBarItem(
+          label: "Profile",
+          icon: Icon(
+            Icons.account_circle,
+          ),
+        ),
+      ],
+      currentIndex: _selectedPage,
+      onTap: (int? value) {
+        if (value != null) {
+          setState(() {
+            _selectedPage = value;
+          });
+        }
+      },
     );
   }
 }
