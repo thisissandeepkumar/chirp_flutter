da3714a585cde39ed6adb33323df52fa3064ab52
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-01-09 11:25:32 +0530

    Responsive added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:comms_flutter/pages/login/index.dart';
 import 'package:comms_flutter/pages/splash/index.dart';
 import 'package:comms_flutter/services/navigation_service.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +19,20 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) => ResponsiveWrapper.builder(
+        child,
+        maxWidth: 1200,
+        minWidth: 480,
+        defaultScale: true,
+        breakpoints: [
+          const ResponsiveBreakpoint.resize(480, name: MOBILE),
+          const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+        ],
+        background: Container(
+          color: const Color(0xFFF5F5F5),
+        ),
+      ),
       navigatorKey: NavigationService.instance.navigatorKey,
       title: 'Comms',
       theme: ThemeData(
